eec2f206842ce0c88be32d361013515a817769fa
zldongly <zldongly@163.com>
2021-08-17 21:46:57 +0800

    kafka_topic

---
+++ api/comment/job/v1/job_kafka.go
@@ -0,0 +1,12 @@
+package v1
+
+// kafka topic
+const (
+	TopicSubjectCreate = "comment_subject_create"
+	TopicSubjectCache  = "comment_subject_cache"
+	TopicCommentCreate = "comment_create"
+	TopicCommentDelete = "comment_delete"
+	TopicIndexCache    = "comment_index_cache"
+	TopicContentCache  = "comment_content_cache"
+	TopicReplyCache    = "comment_reply_cache"
+)
