cbd12713abeac2d13bc8c0dbf2560dc1f8469464
zldongly <zldongly@163.com>
2021-08-19 21:26:02 +0800

    comment_service/pkg

---
+++ app/comment/service/internal/pkg/convert/convert.go
@@ -0,0 +1,33 @@
+package convert
+
+import (
+	"strconv"
+	"strings"
+)
+
+func StringToInt64s(str string) []int64 {
+	ss := strings.Split(str, ",")
+	is := make([]int64, 0, len(ss))
+	for _, s := range ss {
+		i, err := strconv.ParseInt(s, 10, 64)
+		if err != nil {
+			continue
+		}
+		is = append(is, i)
+	}
+
+	return is
+}
+
+func Int64sToString(is []int64) string {
+	var s string
+
+	for idx, i := range is {
+		if idx != 0 {
+			s += ","
+		}
+		s += strconv.FormatInt(i, 10)
+	}
+
+	return s
+}
--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/gomodule/redigo v1.8.5
 	github.com/google/wire v0.5.0
+	github.com/stretchr/testify v1.7.0
 	google.golang.org/genproto v0.0.0-20210629200056-84d6f6074151
 	google.golang.org/grpc v1.39.0
 	google.golang.org/protobuf v1.27.1
