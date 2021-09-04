4ea9f0d44207381ffc7220032eab86b7459c98df
zldongly <zldongly@163.com>
2021-09-04 20:02:12 +0800

    feat(job.data):cacheIndex

--- app/comment/job/internal/data/comment_index.go
+++ app/comment/job/internal/data/comment_index.go
@@ -1,12 +1,16 @@
 package data
 
-import "time"
+import (
+	"context"
+	"fmt"
+	"time"
+)
 
 const (
-	_commentIndexCacheKey           = `comment_index_cache:%d:%d`          // obj_id, obj_type
-	_commentIndexLocalCacheKey      = `comment_index_cache:%d:%d:%d:%d`    // obj_id, obj_type, pageNo, pageSize
-	_commentReplyIndexCacheKey      = `comment_reply_index_cache:%d`       // root_id
-	_commentReplyIndexLocalCacheKey = `comment_reply_index_cache:%d:%d:%d` // root_id, pageNo, pageSize
+	_commentIndexCacheKey      = `comment_index_cache:%d:%d`    // obj_id, obj_type
+	_commentReplyIndexCacheKey = `comment_reply_index_cache:%d` // root_id
+
+	_commentIndexCacheTtl = 8 * 60 * 60 // 8h
 )
 
 type CommentIndex struct {
@@ -35,4 +39,56 @@ type CommentIndex struct {
 
 func (*CommentIndex) TableName() string {
 	return "comment_index"
+}
+
+func (r *commentRepo) CacheIndex(ctx context.Context, objId int64, objType int32, pageNo, pageSize int32) error {
+	var (
+		key    = fmt.Sprintf(_commentIndexCacheKey, objId, objType)
+		redis  = r.data.redis.Get()
+		log    = r.log
+		offset int64                             // db offset
+		count  = int64(pageNo) * int64(pageSize) // 总数
+		indexs []*CommentIndex
+	)
+	defer redis.Close()
+
+	// 是否存在key，如果存在直接延时
+	reply, err := redis.Do("expire", key, _commentIndexCacheTtl)
+	if err != nil {
+		return err
+	}
+	if res, ok := reply.(int64); ok && res == 1 { // 存在 key
+		reply, err = redis.Do("zcard", key) // 现存index数量
+		if err != nil {
+			return err
+		}
+		offset, _ = reply.(int64)
+		if offset >= count {
+			return nil
+		}
+	}
+
+	// 查db
+	result := r.data.db.
+		WithContext(ctx).
+		Where("obj_id = ?", objId).
+		Where("obj_type = ?", objType).
+		Where("root = ?", 0).
+		Order("create_at DESC").
+		Offset(int(offset)).
+		Limit(int(count - offset)).
+		Find(&indexs)
+	if result.Error != nil {
+		return result.Error
+	}
+
+	// 写入redis
+	for _, index := range indexs {
+		_, err = redis.Do("zadd", key, index.CreateAt.Unix(), index.Id)
+		if err != nil {
+			log.Error(err)
+		}
+	}
+
+	return nil
 }
